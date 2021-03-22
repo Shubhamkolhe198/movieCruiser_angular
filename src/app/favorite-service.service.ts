c69720294987032cd5c7ca6080fa48ee23c82e86
Shubham Kolhe <shubhamkolhe198@gmail.com>
2021-03-22 16:39:53 +0530

    first commit

--- src/app/favorite-service.service.ts
+++ src/app/favorite-service.service.ts
@@ -9,7 +9,7 @@ export class FavoriteServiceService {
   constructor(private http:HttpClient) { }
   getFavoritesList():Observable<any>{
     return this.http.get<any>("http://localhost:8081/favorite-ang",{
-      headers: new HttpHeaders().set('Authorization',"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiaWF0IjoxNjE1OTE2MjIwLCJleHAiOjE2MTYxNjY1NDh9.zSqfWaixXXCRSL0drIn6Fi5MpeOFCCrUGj42ve-JsPA")
+      headers: new HttpHeaders().set('Authorization',"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiaWF0IjoxNjE1OTYwNzg3LCJleHAiOjE2MTYyMTExMTR9.XKIIZwpKXfBNu1edzCT2Lu8nhP56unh3d0yF6IL_B8M")
      }
      ).pipe();
    }
@@ -17,7 +17,7 @@ export class FavoriteServiceService {
     deleteFavorite(id:number){
       const deleteUrl = (`http://localhost:8081/delete-ang/${id}`);
       return this.http.delete(deleteUrl,{
-        headers: new HttpHeaders().set('Authorization',"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiaWF0IjoxNjE1OTE2MjIwLCJleHAiOjE2MTYxNjY1NDh9.zSqfWaixXXCRSL0drIn6Fi5MpeOFCCrUGj42ve-JsPA")
+        headers: new HttpHeaders().set('Authorization',"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiaWF0IjoxNjE1OTYwNzg3LCJleHAiOjE2MTYyMTExMTR9.XKIIZwpKXfBNu1edzCT2Lu8nhP56unh3d0yF6IL_B8M")
       });
     }
   
